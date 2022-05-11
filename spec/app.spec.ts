d75ed4b185984b0603982c88b0b4922ad76efac2
Horatzio <horatiuduma@gmail.com>
2022-05-11 15:55:19 +0300

    Project Setup with @serenity-js & Protractor

---
+++ spec/app.spec.ts
@@ -0,0 +1,12 @@
+import "jasmine";
+import { actorCalled } from "@serenity-js/core";
+import { BrowseTheWeb } from "@serenity-js/protractor";
+import { protractor } from "protractor";
+import { OpenTheApp } from "./OpenTheApp";
+
+describe("Lighthouse App", () => {
+  it("lets us book desks", () =>
+    actorCalled("End User")
+      .whoCan(BrowseTheWeb.using(protractor.browser))
+      .attemptsTo(OpenTheApp()));
+});
