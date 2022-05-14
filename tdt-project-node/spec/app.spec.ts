466a1ebf3c82bfc52253915467626f959efb5f16
Horatzio <horatiuduma@gmail.com>
2022-05-14 13:06:29 +0300

    Cleaned up dependencies. Used Jasmine instead of Protractor.

--- tdt-project-node/spec/app.spec.ts
+++ tdt-project-node/spec/app.spec.ts
@@ -1,8 +1,10 @@
 import "jasmine";
 import assert = require("assert");
+import { launch } from "puppeteer";
 
 describe("Lighthouse App", () => {
-  it("lets us book desks", () => {
+  it("lets us book desks", async () => {
     assert(true);
+    const pup = await launch({});
   });
 });
